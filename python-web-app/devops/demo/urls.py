51d0d781bc31be4f086db3a9a82305d32486caa5
vamshichereddy <vamshich2022@gmail.com>
2025-03-28 18:17:24 +0530

    Intial Commit

--- python-web-app/devops/demo/urls.py
+++ python-web-app/devops/demo/urls.py
@@ -1,7 +1,25 @@
-from django.urls import path
+"""devops URL Configuration
 
-from . import views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from demo import views  # Import views from the demo app
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),  # Use 'index' to match your views.py
+    path('demo/', include('demo.urls')),  # Include the demo app URLs
+    path('admin/', admin.site.urls),  # Admin panel
 ]
+
